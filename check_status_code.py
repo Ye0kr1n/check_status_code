ca99ff5d002b4a86399d5f993c72c0183731ab6e
Ye0kr1n <1005406456@qq.com>
2020-10-30 10:18:56 +0800

    Update check_status_code.py
    修复输出数据格式问题

--- check_status_code.py
+++ check_status_code.py
@@ -16,6 +16,7 @@ def read_xls(file):
     dataFile = []
     ranges=0
     host=""
+    print("[*]Data importing..........")
     for rowNum in range(table.nrows):
         if rowNum > 0:
             if table.row_values(rowNum)[1].find('-')==-1:       #连续端口导入
@@ -25,6 +26,7 @@ def read_xls(file):
                 ranges=table.row_values(rowNum)[1].split('-')
                 for i in range(int(ranges[0]),int(ranges[1])+1,1):
                     dataFile.append([host,str(i)])
+    print("[+]Data import over")
     return dataFile,len(dataFile)
 def get_title(url):
     res=requests.get(url).content
@@ -42,7 +44,7 @@ def write_xls(data):
         sheet1.write(i+1, 0, data[i][0])    #a[n][0]是ip地址,a[n][1]是响应码状态,a[n][2]是网页标题
         sheet1.write(i+1, 1, data[i][1])
         sheet1.write(i+1, 2, data[i][2])
-    f.save('write_status.xls')      #输出的文件名 write_status.xls
+    f.save('write_status.xls')
 def check_res_code(url):
     res=""
     u=url[0]
@@ -50,33 +52,38 @@ def check_res_code(url):
     url="http://"+u+":"+p
     urls="https://"+u+":"+p
     title=""
+    url_ans=""
     try:
-        res=str(requests.get(url,timeout=3).status_code)    #此处做了http和https两种请求模式,取响应码
+        res=str(requests.get(url,timeout=0.5).status_code)    #此处做了http和https两种请求模式,取响应码
+        url_ans=url
         title=get_title(url)
     except:
         try:
-            res=str(requests.get(urls,timeout=3).status_code)
+            res=str(requests.get(urls,timeout=0.5).status_code)
+            url_ans = urls
             title=get_title(urls)
         except:
-            urls=u+":"+p
+            url_ans=u+":"+p
             res="-1"
             title="Timed out"
-    return [urls,res,title]
+    return [url_ans,res,title]
 
 if __name__ == '__main__':
-    readlFile = '1.xlsx'  #读取的文件名
+    readlFile = '1.xlsx'
     url_list = read_xls(readlFile)[0]
     total=read_xls(readlFile)[1]
     a=[]
-    print("Total:%d,timeout default:3,check status start now!!!!" %total)
+    print("[+]Total:%d,timeout default:3,check status start now!!!!" %total)
     for i in range(0,len(url_list),1):
         a.append(check_res_code(url_list[i]))
     #    print(check_res_code(url_list[i]))
-        print("[%s/%s]%s   status code:%s    title:%s"%(i+1,total,check_res_code(url_list[i])[0],check_res_code(url_list[i])[1],check_res_code(url_list[i])[2]))
+        print("[*][%s/%s]%s   status code:%s    title:%s"%(i+1,total,check_res_code(url_list[i])[0],check_res_code(url_list[i])[1],check_res_code(url_list[i])[2]))
     que=input("All over!!!Output to xls?[Y/n]")
     if que=='Y' or que=='':
-        print("Generating XLS files now......")
-        write_xls(a)    #xls文件输出
-        print("Write OK!!!")
+        print("[*]Generating XLS files now......")
+        write_xls(a)
+        print("[+]Write OK!!!")
     else:
         exit()
+
+
